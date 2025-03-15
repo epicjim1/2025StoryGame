5c2521fb4e5567e443dd4e7d599c2a454b8f6651
Abhishek Luthra <abhishekluthra2005@gmail.com>
2025-03-15 19:53:29 -0400

    Working animation with movement

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace FinalCharacterController
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private float locomotionBlendSpeed = 4f;
+
+        private PlayerLocomotionInput playerLocomotionInput;
+        private PlayerState playerState;
+
+        private static int inputXHash = Animator.StringToHash("inputX");
+        private static int inputYHash = Animator.StringToHash("inputY");
+        private static int inputMagnitudeHash = Animator.StringToHash("inputMagnitude");
+
+        private Vector3 currentBlendInput = Vector3.zero;
+
+        private void Awake()
+        {
+            playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+            playerState = GetComponent<PlayerState>();
+        }
+
+        private void Update()
+        {
+            UpdateAnimationState();
+        }
+
+        private void UpdateAnimationState()
+        {
+            bool isSprinting = playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+
+            Vector2 inputTarget = isSprinting ? playerLocomotionInput.MovementInput * 1.5f : playerLocomotionInput.MovementInput;
+            currentBlendInput = Vector3.Lerp(currentBlendInput, inputTarget, locomotionBlendSpeed * Time.deltaTime);
+
+            animator.SetFloat(inputXHash, currentBlendInput.x);
+            animator.SetFloat(inputYHash, currentBlendInput.y);
+            animator.SetFloat(inputMagnitudeHash, currentBlendInput.magnitude);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ namespace FinalCharacterController
     [DefaultExecutionOrder(-1)]
     public class PlayerController : MonoBehaviour
     {
+        #region Class Variables
         [Header("Components")]
         [SerializeField] private CharacterController characterController;
         [SerializeField] private Camera playerCamera;
@@ -12,7 +13,10 @@ namespace FinalCharacterController
         [Header("Base Movement")]
         public float runAcceleration = 0.25f;
         public float runSpeed = 4f;
+        public float sprintAccelerataion = 50f;
+        public float sprintSpeed = 7f;
         public float drag = 0.1f;
+        public float movingThreshold = 0.01f;
 
         [Header("Camera Settings")]
         public float lookSenseH = 0.1f;
@@ -20,14 +24,21 @@ namespace FinalCharacterController
         public float lookLimitV = 89f;
 
         private PlayerLocomotionInput playerLocomotionInput;
+        private PlayerState playerState;
+
         private Vector2 cameraRotation = Vector2.zero;
         private Vector2 playerTargetRotation = Vector2.zero;
+        #endregion
 
+        #region Startup
         private void Awake()
         {
             playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+            playerState = GetComponent<PlayerState>();
         }
+        #endregion
 
+        #region Update Logic
         private void Update()
         {
             if (DialogueManager.GetInstance().dialogueIsPlaying)
@@ -35,21 +46,50 @@ namespace FinalCharacterController
                 return;
             }
 
+            UpdateMovementState();
+            HandleLateralMovement();
+        }
+
+        private void UpdateMovementState()
+        {
+            bool isMovementInput = playerLocomotionInput.MovementInput != Vector2.zero;    //order
+            bool isMovingLaterally = IsMovingLaterally();                                   //matter
+            bool isSprinting = playerLocomotionInput.SprintToggledOn && isMovingLaterally; //order matters
+            //Debug.Log(isMovingLaterally);
+
+            PlayerMovementState lateralState = isSprinting ? PlayerMovementState.Sprinting :
+                                               isMovingLaterally || isMovementInput ? PlayerMovementState.Running : PlayerMovementState.Idling;
+
+            playerState.SetPlayerMovementState(lateralState);
+        }
+        private Vector3 newVelocity = Vector3.zero;
+        private void HandleLateralMovement()
+        {
+            // Create quick references for current state
+            bool isSprinting = playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+
+            float lateralAcceleration = isSprinting ? sprintAccelerataion : runAcceleration;
+            float clampLateralMagnitude = isSprinting ? sprintSpeed : runSpeed;
+
+            // State dependent acceleration and speed
             Vector3 cameraForwardXZ = new Vector3(playerCamera.transform.forward.x, 0f, playerCamera.transform.forward.z).normalized;
             Vector3 cameraRightXZ = new Vector3(playerCamera.transform.right.x, 0f, playerCamera.transform.right.z).normalized;
             Vector3 movementDirection = cameraRightXZ * playerLocomotionInput.MovementInput.x + cameraForwardXZ * playerLocomotionInput.MovementInput.y;
 
-            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
-            Vector3 newVelocity = characterController.velocity + movementDelta;
+            Vector3 movementDelta = movementDirection * lateralAcceleration;
+             newVelocity += movementDelta;//= characterController.velocity + movementDelta;
 
-            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
-            newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
-            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
+            // Add drag to player
+            Vector3 currentDrag = newVelocity.normalized * drag;
+            newVelocity = (newVelocity.magnitude > drag) ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, clampLateralMagnitude);
 
             // Move character only use once
             characterController.Move(newVelocity * Time.deltaTime);
         }
+        #endregion
 
+        #region Late Update Logic
         private void LateUpdate()
         {
             if (DialogueManager.GetInstance().dialogueIsPlaying)
@@ -65,6 +105,16 @@ namespace FinalCharacterController
 
             playerCamera.transform.rotation = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0f);
         }
+        #endregion
+
+        #region State Checks
+        private bool IsMovingLaterally()
+        {
+            Vector3 lateralVelocity = new Vector3(newVelocity.x, 0f, newVelocity.z);
+            //Debug.Log(lateralVelocity);
+            return lateralVelocity.magnitude > movingThreshold;
+        }
+        #endregion
 
         private void OnTriggerStay(Collider other)
         {
